a9e5e2d025e0c6299ac685abbccf788e490a7b6b
Greg Guthe <gregg@aweber.com>
2013-01-28 21:10:31 -0500

    zsh: Remove nodey-tools directory from completion
    Use npm link to test local version.

--- .nodey.zsh
+++ .nodey.zsh
@@ -1,5 +1,5 @@
 # completion for nodey-tools
 function nt_avail {
-    reply=(`~/nodey-tools/bin/nodey-tools --quiet list`)
+    reply=(`nodey-tools --quiet list`)
 }
 compctl -K nt_avail nodey-tools
