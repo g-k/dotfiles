0e47c41fdefc193ffca3f541fa2ef535189fd8d3
Greg Guthe <greg.guthe@gmail.com>
2014-04-26 16:19:06 -0400

    global osx homebrew powerline setup

--- .my-zshrc.sh
+++ .my-zshrc.sh
@@ -1,3 +1,7 @@
+if [[ -r /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
+    source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+fi
+
 if [[ -r ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
     source ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
 fi
