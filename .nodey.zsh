5a062148534975144b727f520d23fefed355b271
Greg Guthe <greg.guthe@gmail.com>
2021-06-25 14:10:01 -0400

    remove unused dotfiles

--- .nodey.zsh
+++
@@ -1,5 +0,0 @@
-# completion for nodey-tools
-function nt_avail {
-    reply=(`nodey-tools --quiet list`)
-}
-compctl -K nt_avail nodey-tools
--- .xmonad/xmonad.hs
+++
@@ -1,7 +0,0 @@
-import XMonad
-import XMonad.Hooks.ManageDocks
-
-main = xmonad $ defaultConfig {
-        manageHook = manageDocks <+> manageHook defaultConfig,
-        layoutHook = avoidStruts  $  layoutHook defaultConfig
-	}
