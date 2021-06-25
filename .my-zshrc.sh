10a224f462fb5e2b4a85ada872da7f50da4af488
Greg Guthe <greg.guthe@gmail.com>
2021-06-25 15:25:37 -0400

    zsh: switch to starship for prompt and PS1

--- .my-zshrc.sh
+++ .my-zshrc.sh
@@ -1,13 +1,4 @@
-which prompt &> /dev/null && prompt minimal
-
-
-if [[ -r /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
-    source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
-fi
-
-if [[ -r ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
-    source ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
-fi
+eval "$(starship init zsh)"
 
 # set PATH
 CABAL="$HOME/.cabal/bin"
